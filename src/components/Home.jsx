187ca0134a8ab76f6d7aecf958fd22187bb87029
SamBatula <sobatula@syr.edu>
2023-06-16 15:25:20 -0400

    update Version 1.3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,27 +10,37 @@ Github Grey:
 */
 
 // typing rafce generates the skeleton functional component below
-import React from 'react'
+import React, {useState} from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
 import {Link} from 'react-scroll'
 
 
 const Home = () => {
-    
+
+    const[home, setHome] = useState(false)
+    {/* click function handleClick to setNav to the opposite of its current value */}
+    const handleClick = () => setHome(!home)
+
   return (
     <div name='home' className='w-full h-screen bg-[#023020]'>
 
-    {/* Container */}
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+    {/* Parent Container*/}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full '>
             <h1 className= 'text-4xl sm:text-7xl font-bold text-[#d9b99b]'> Hi, I'm Samson!</h1>
             <h2 className='text-2xl sm:text-2xl font-bold text-[#ccd6f6]'> A Software Developer. </h2>
+
+    {/* About Me and on Click container w/ arrow animation using link react-scroll library */}
             <div>
-                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#355E3B] hover:border-[#355E3B]'> About Me 
+            <Link onClick = {handleClick} to="about"  smooth={true} offset={50} duration={500}>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#355E3B] hover:border-[#355E3B]'>About Me
+                    
                     <span className='group-hover:rotate-90 duration-200'>
                         
                         <HiArrowNarrowRight className='ml-3'/> 
                     </span>
                 </button>
+            </Link>
+                
             </div>
         </div>
         
